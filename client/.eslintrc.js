99e3f915ff56aa9e93b865e3c059930f8f78f1dd
AngeEf <efa.letter@gmail.com>
2022-10-20 15:37:53 +0300

    init

---
+++ client/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/no-unresolved': 0,
+    'max-len': 0,
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/jsx-no-useless-fragment': 0,
+  },
+};
