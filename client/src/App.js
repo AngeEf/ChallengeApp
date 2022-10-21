f3b3e71a88beacfc678c4cf7d75c2d7e3f3a84bb
AndreyKonkin <parenekrus34@mail.ru>
2022-10-21 10:00:34 +0300

    merge_3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+// import { Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import MainPage from './components/MainPage/MainPage';
 import CategotyView from './components/CategoryView/CategoryView';
